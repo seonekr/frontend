8cd1b9e72d49ace2fa22c0c28ddc36d103a6670f
seonekr <zaemuevln42gmail.com>
2024-01-15 12:52:57 +0700

    try

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SellerRegister from './components/sellerRegister/SellerRegister';
+import RegisterUser from './components/loginAndSignup/RegisterUser'
+import LoginUser from './components/loginAndSignup/LoginUser';
 
-  return (
-    <>
-    <h1>App</h1>
-    </>
-  )
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' Component={SellerRegister}/>
+                <Route path='/login-user' Component={LoginUser}/>
+                <Route path='/register-user' Component={RegisterUser}/>
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
