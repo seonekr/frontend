651c91894afa79d1c90aac8759eb2baf92406cce
phukeolaeneeku <144010043+phukeolaeneeku@users.noreply.github.com>
2024-01-15 12:52:15 +0700

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SignUpUser from './components/SignUpUser';
-import LoginUser from './components/loginAndSignup/LoginUser';
-import RegisterUser from './components/loginAndSignup/RegisterUser';
-import ForgotPassword from './components/forgotPassword/ForgotPassword';
-import UserRegister from './components/userRegister/UserRegister';
-import SellerRegister from './components/sellerRegister/SellerRegister';
-import SigupGoogle from './components/sigupforGoogle/SigupGoogle';
-import AdditionalSeller from './components/additionalSeller/AdditionalSeller';
-import More from './components/more/More';
-import Home from './components/Home';
-import ProductDetial from './components/products/ProductDetial';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path='/signupuser' Component={SignUpUser}/>
-                <Route path='/loginuser' Component={LoginUser}/>
-                <Route path='/registerUser' Component={RegisterUser}/>
-                <Route path='/forgotPassword' Component={ForgotPassword}/>
-                <Route path='/userRegister' Component={UserRegister}/>
-                <Route path='/sellerRegister' Component={SellerRegister}/>
-                <Route path='/sigupGoogle' Component={SigupGoogle}/>
-                <Route path='/additionalSeller' Component={AdditionalSeller}/>
-                <Route path='/more' Component={More}/>
-                <Route path='/' Component={Home}/>
-                <Route path='/productdetial' Component={ProductDetial}/>
-            </Routes>
-        </Router>
-    );
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+  </>,
+)
