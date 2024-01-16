b05344ea9017c4fe337b2665cc08c0d90068ddc9
seonekr <zaemuevln42gmail.com>
2024-01-16 08:51:15 +0700

    delete api

--- src/api/agree.js
+++
@@ -1,8 +0,0 @@
-import { axiosCredentials } from "util/util";
-
-export function PolicyAPI(number) {
-  return axiosCredentials(
-    "get",
-    `${process.env.REACT_APP_BACKEND_API_URL}/store/terms/${number}`
-  );
-}
