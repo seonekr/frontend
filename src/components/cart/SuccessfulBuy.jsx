597820387e978373fc08a4d72f08af8a663932be
seonekr <93129992+KONGCHAN-999@users.noreply.github.com>
2024-01-16 15:34:24 +0700

    Update

--- src/components/cart/SuccessfulBuy.jsx
+++ src/components/cart/SuccessfulBuy.jsx
@@ -1,67 +1,64 @@
 import React from 'react'
 import './SuccessfulBuy.css'
-import { FaAngleLeft } from "react-icons/fa6";
+import Header from "../header/Header";
+import Menu from "../menuFooter/Menu";
 import { Link } from "react-router-dom";
-import { ImCheckmark } from 'react-icons/im';
+import { IoIosArrowBack } from "react-icons/io";
 
 function SuccessfulBuy() {
     return (
-        <form id='menager'>
-            <div className="group_lisblecackk">
-                <div className="box_guopoidHead">
-                    <div className="idf">
-                        <p>NO: 15</p>
-                        <p>ID: 4</p>
-                        <p>Name: Acer</p>
-                    </div>
-                    <div className='box_icon_Check'>
-                        <ImCheckmark id='icon_ImCheckmark' />
-                    </div>
-                </div>
-
-                <div className='box_bill_productS'>
-                    <hr />
-                    <div className='bill_product_name'><h2>Products</h2></div>
-
-                    <div className="order_billGopBox">
-                        <div className="detial">
-                            <h3>Product Name</h3>
-                            <p>name...</p>
-                            <p>name...</p>
-                            <p>name...</p>
-                        </div>
-                        <div className="detial">
-                            <h3>Price</h3>
-                            <p>$20.00</p>
-                            <p>$20.00</p>
-                            <p>$20.00</p>
-                        </div>
-                        <div className="detial">
-                            <h3>Amount</h3>
-                            <p>5</p>
-                            <p>5</p>
-                            <p>5</p>
-                        </div>
-                    </div>
-                    <hr />
-                    <div className='box_title_product'>
-                        <div className="box_titlePrice">
-                            <h3>TOTAL:</h3>
-                            <p>$100.00</p>
-                        </div>
-                        <div className="box_place-on">
-                            <p>Place on: 15/09/2023</p>
-                            <p>Payment method: Bcel One</p>
-                            <p>Status: completed</p>
-                            <p>Delivery by: Anousit</p>
-                        </div>
-                    </div>
-                </div>
-                <Link to="/product_search">
-                    <button className="btnViewProduct" >Go To Shop More</button>
+        <>
+            <Header />
+            <div className="bill">
+                <Link to="/" className="box_container_back_icons_back">
+                    <IoIosArrowBack id="icons_back" />
+                    <p>Back</p>
                 </Link>
+                <div className="bill-detial newspanBox" >
+                    <div className="guopoidHead">
+                        <div className="idf">
+                            <p>OrderID: </p>
+                            <p>UserID: </p>
+                            <p>Name: </p>
+                        </div>
+                    </div>
+                    <div className="billGopBox">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Product Name</th>
+                                    <th>Price</th>
+                                    <th>Amount</th>
+                                    <th>Color</th>
+                                    <th>Size</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>name</td>
+                                    <td>2,000</td>
+                                    <td>3</td>
+                                    <td>red</td>
+                                    <td>L</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="titlePrice">
+                        <p>Total:</p>
+                        <p>2,000 KIP</p>
+                    </div>
+                    <div className="place-on">
+                        <p>Place on: BCEL ONE</p>
+                        <p>Payment method: MasterCard</p>
+                        <p>Status: something</p>
+                        <p>Delivery: company name</p>
+                    </div>
+                </div>
+                
             </div>
-        </form>
+            <Menu />
+        </>
     )
 }
 
