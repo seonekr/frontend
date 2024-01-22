ad3e398497521a2b1f3607687fd097bdda4cb2db
phukeolaeneeku <144010043+phukeolaeneeku@users.noreply.github.com>
2024-01-22 09:25:45 +0700

    Update

--- src/components/userRegister/UserRegister.jsx
+++ src/components/userRegister/UserRegister.jsx
@@ -1,107 +1,243 @@
+// import { useState } from "react";
+// import "./userRegister.css"
+// import { Link } from 'react-router-dom';
+// import { MdArrowBack } from "react-icons/md";
+// import axios from "axios";
+
+// const UserRegister = () => {
+//   const [formData, setFormData] = useState({
+//     email: "",
+//     certificationNumber: "",
+//     nickname: "",
+//     password: "",
+//     verifyPassword: "",
+//   });
+//   const [message, setMessage] = useState("");
+//   const [passwordMatch, setPasswordMatch] = useState(true);
+
+//   const email = formData.email;
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setFormData({ ...formData, [name]: value });
+//   };
+
+//   const handleSendVerification = async () => {
+//     try {
+//       const response = await axios.post(
+//         "https://api.web4all.site/user/send-email",
+//         { email }
+//       );
+
+//       setMessage(response.data.message);
+//     } catch (error) {
+//       console.error("Error sending email:", error.message);
+//       setMessage("Error sending email. Please try again.");
+//     }
+//   };
+
+//   const handleRegister = async () => {
+//     if (formData.password !== formData.verifyPassword) {
+//       setPasswordMatch(false);
+//       return;
+//     }
+
+//     await axios({
+//       method: "post",
+//       url: "https://api.web4all.site/user/signup",
+//       data: formData,
+//     }).then((response) => {
+//       console.log(response.data);
+//     });
+//   };
+
+
+
+//   return (
+//     <>
+//         <div className="box_forgot">
+//           <Link to="/registeruser" className="box_iconBack">
+//             <MdArrowBack id='iconBack'/>
+//           </Link>
+
+//             <h2>User registratio</h2>
+//             <div className="title">You are in the process of signing up as a user!</div>
+//             <form className='container_form_user'>
+//               <div className='box_title'>Enter basic information</div>
+
+//               <div className='container_form_user2'>
+//                 <input 
+//                   type="email" 
+//                   placeholder="Email" 
+//                   required 
+//                   onChange={handleChange}
+//                 />
+//                 <div className='verification' onClick={handleSendVerification} >Ceritification</div>
+//               </div>
+
+//               <input 
+//                 type="text" 
+//                 placeholder="Certication Number" 
+//                 required 
+//                 onChange={handleChange}
+//               />
+
+//               <input 
+//                 type="text" 
+//                 placeholder="Nickname (maximun 10 characters)" 
+//                 required 
+//                 onChange={handleChange}
+//               />
+
+//               <input 
+//                 type="password" 
+//                 placeholder="Passwords" 
+//                 required 
+//                 onChange={handleChange}
+//               />
+
+//               <input 
+//                 type="password" 
+//                 placeholder="Verify password" 
+//                 required 
+//                 onChange={handleChange}
+//               />
+//                {!passwordMatch && (
+//                   <p className="error-text">Passwords do not match.</p>
+//                   )}
+//                   <button type="button" onClick={handleRegister}>
+//                     Register
+//                   </button>
+//                 {message && <p>{message}</p>}
+          
+//             </form>
+//         </div>
+//     </>
+//   );
+// };
+
+// export default UserRegister;
+
 import { useState } from "react";
-import "./userRegister.css"
-import { Link } from 'react-router-dom';
+import "./userRegister.css";
+import { Link } from "react-router-dom";
 import { MdArrowBack } from "react-icons/md";
+import axios from "axios";
 
 const UserRegister = () => {
   const [formData, setFormData] = useState({
-    email: '',
-    certificationNumber: '',
-    nickname: '',
-    password: '',
-    verifyPassword: '',
+    email: "",
+    certificationNumber: "",
+    nickname: "",
+    password: "",
+    verifyPassword: "",
   });
-
-
+  const [message, setMessage] = useState("");
   const [passwordMatch, setPasswordMatch] = useState(true);
+
+  const email = formData.email;
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSendVerification = async () => {
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/user/send-email",
+        { email }
+      );
 
+      setMessage(response.data.message);
+    } catch (error) {
+      console.error("Error sending email:", error.message);
+      setMessage("Error sending email. Please try again.");
+    }
+  };
+
+  const handleRegister = async () => {
     if (formData.password !== formData.verifyPassword) {
       setPasswordMatch(false);
       return;
     }
 
-    try {
-
-      // const response = await axios.post('http://localhost:8000/api/user/', formData);
-
-      // console.log('API Response:', response.data);
-
-      console.log(formData)
-      setFormData({
-        email: '',
-        certificationNumber: '',
-        nickname: '',
-        password: '',
-        verifyPassword: '',
-      });
-    } catch (error) {
-
-      console.error('Error submitting form:', error);
-    }
+    await axios({
+      method: "post",
+      url: "http://127.0.0.1:8000/user/signup",
+      data: formData,
+    }).then((response) => {
+      console.log(response.data);
+    });
   };
-
-
- 
-
 
   return (
     <>
-        <div className="box_forgot">
-          <Link to="/registeruser" className="box_iconBack">
-            <MdArrowBack id='iconBack'/>
-          </Link>
+      <div className="box_forgot">
+        <Link to="/registeruser" className="box_iconBack">
+          <MdArrowBack id="iconBack" />
+        </Link>
 
-            <h2>User registratio</h2>
-            <div className="title">You are in the process of signing up as a user!</div>
-            <form className='container_form_user'>
-              <div className='box_title'>Enter basic information</div>
-
-              <div className='container_form_user2'>
-                <input 
-                  type="email" 
-                  placeholder="Email" 
-                  required 
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <div className='verification' onClick={handleRegistration} >Ceritification</div>
-              </div>
-
-              <input 
-                type="text" 
-                placeholder="Certication Number" 
-                required 
-              />
-
-              <input 
-                type="text" 
-                placeholder="Nickname (maximun 10 characters)" 
-                required 
-              />
-
-              <input 
-                type="password" 
-                placeholder="Passwords" 
-                required 
-              />
-
-              <input 
-                type="password" 
-                placeholder="Verify password" 
-                required 
-              />
-              
-              <button type="submit">Check</button>
-            </form>
+        <h2>User registration</h2>
+        <div className="title">
+          You are in the process of signing up as a user!
         </div>
+        <div className="container_form_user">
+          <div className="box_title">Enter basic information</div>
+          <div className='container_form_user2'>
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              value={formData.email}
+              placeholder="Email"
+              required
+            />
+            <div className='verification' onClick={handleSendVerification}>
+              Send Verification Email
+            </div>
+          </div>
+          <input
+            type="text"
+            name="certificationNumber"
+            onChange={handleChange}
+            value={formData.certificationNumber}
+            placeholder="Certication Number"
+            required
+          />
+          <input
+            type="text"
+            name="nickname"
+            onChange={handleChange}
+            value={formData.nickname}
+            placeholder="Nickname (maximun 10 characters)"
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            value={formData.password}
+            placeholder="passwords"
+            required
+          />
+          <input
+            type="password"
+            name="verifyPassword"
+            onChange={handleChange}
+            value={formData.verifyPassword}
+            placeholder="Verify password"
+            required
+          />
+          {!passwordMatch && (
+            <p className="error-text">Passwords do not match.</p>
+          )}
+          <button type="button" onClick={handleRegister}>
+            Register
+          </button>
+        </div>
+      </div>
+      {message && <p>{message}</p>}
     </>
   );
 };
