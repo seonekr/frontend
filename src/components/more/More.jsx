9c2048aeff6fdb88545a72d04485c89f4c404d8f
seonekr <93129992+KONGCHAN-999@users.noreply.github.com>
2024-01-23 09:31:03 +0700

    Update

--- src/components/more/More.jsx
+++ src/components/more/More.jsx
@@ -5,11 +5,22 @@ import { IoLogOutOutline } from "react-icons/io5";
 import { IoKeySharp } from "react-icons/io5";
 import { BsBackpack4Fill } from "react-icons/bs";
 import { IoPeopleCircle } from "react-icons/io5";
+import { IoIosArrowBack } from "react-icons/io";
 import { Link } from 'react-router-dom';
 
 export const More = () => {
   return (
     <>
+      <div className="header_box_management">
+        <Link to="/" className="box_management_iconnback">
+          <IoIosArrowBack id="icons_back" />
+          <p>Back</p>
+        </Link>
+        <div>
+          <h3>Setting Account</h3>
+        </div>
+        <div></div>
+      </div>
       <div className="MorePage">
         <div className="profile_box">
           <div className="left_box">
@@ -23,33 +34,33 @@ export const More = () => {
           </Link>
         </div>
 
-        <hr className='hr'/>
+        <hr className='hr' />
         <div className="more-menu-list">
-          <div className='menu_icon'> 
-            <BsBackpack4Fill id="icon_more"/>
+          <div className='menu_icon'>
+            <BsBackpack4Fill id="icon_more" />
             <p>Terms of use</p>
           </div>
-          <hr className='hr'/>
-          <div className='menu_icon'> 
-            <BsBackpack4Fill id="icon_more"/>
+          <hr className='hr' />
+          <div className='menu_icon'>
+            <BsBackpack4Fill id="icon_more" />
             <p>Privay Policy</p>
           </div>
-          <hr className='hr'/>
+          <hr className='hr' />
           <div className='menu_icon'>
-            <IoKeySharp id="icon_more"/>
+            <IoKeySharp id="icon_more" />
             <p>Change password</p>
           </div>
-          <hr className='hr'/>
+          <hr className='hr' />
           <div className='menu_icon'>
-            <IoLogOutOutline id="icon_more"/>
+            <IoLogOutOutline id="icon_more" />
             <p>Log out </p>
           </div>
-          <hr className='hr'/>
+          <hr className='hr' />
           <div className='menu_icon'>
-            <MdDelete id="icon_more"/>
+            <MdDelete id="icon_more" />
             <p>Delete account</p>
           </div>
-          <hr className='hr'/>
+          <hr className='hr' />
         </div>
       </div>
     </>
