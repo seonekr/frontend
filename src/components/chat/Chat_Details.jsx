0c2cd8acde8fa13f42825557926939750fac777a
seonekr <zaemuevln42gmail.com>
2024-01-25 15:59:45 +0700

    Chat room

--- src/components/chat/Chat_Details.jsx
+++ src/components/chat/Chat_Details.jsx
@@ -1,90 +1,109 @@
-import React, { useRef, useState, useEffect } from 'react'
-import User from '../../img/user.png'
-import './chat_Details.css'
+import React, { useRef, useState, useEffect } from "react";
+import User from "../../img/user.png";
+import { VscSend } from "react-icons/vsc";
+import "./chat_Details.css";
 
 function Chat_Details() {
+  const [message, setMessage] = useState("");
+  const [user] = useState("u421")
+  const [store] = useState("s412")
+  const handleInputChange = (event) => {
+    setMessage(event.target.value);
+    autoGrowTextarea();
+  };
 
-    const[message, setMessage] = useState('')
-    const handleInputChange = (event) => {
-        setMessage(event.target.value);
-      };
+  //   box size grow up
+  const autoGrowTextarea = () => {
+    const textarea = document.getElementById("multiline-input");
+    const maxHeight = (8 * window.innerHeight) / 100;
+
+    textarea.style.height = "auto";
+    textarea.style.height = Math.min(textarea.scrollHeight, maxHeight) + "px";
+  };
+
+
+  const handleChat = (e) => {
+    e.preventDefault();
+
+    try {
+
+        const ms = {
+            user: user,
+            message: message,
+            store: store
+        }
     
-      useEffect(() => {
-        const textarea = document.getElementById('multiline-input');
-        textarea.style.height = 'auto';
-        const newHeight = `${textarea.scrollHeight}px`;
-        textarea.style.height = newHeight;
-        textarea.scrollTop = textarea.scrollHeight;
-      }, [message]);
+      console.log(ms)
 
-    return (
-        <>
-            <div className="box_chatDetails_container">
-                <div className="box_chatDetails_details">
-                    <img src={User} alt="image" />
-                    <div className='txt_chat_boxdetails'>
-                        <h4>Sompong</h4>
-                    </div>
-                </div>
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-                <form className='message-form'>
-                    <div className="box_chat_content_room">
-
-                        <div className="sender">
-                            <span className="text-box">Hi, I'm sender</span>
-                        </div>
-
-                        <div className="recipient ">
-                            <span className="text-box">Hi, I'm recipient</span>
-                        </div>
-                        <div className="sender">
-                            <span className="text-box">Hi, I'm sender</span>
-                        </div>
-
-                        <div className="recipient ">
-                            <span className="text-box">Hi, I'm recipient</span>
-                        </div>
-                        <div className="sender">
-                            <span className="text-box">Hi, I'm sender</span>
-                        </div>
-
-                        <div className="recipient ">
-                            <span className="text-box">Hi, I'm recipient</span>
-                        </div>
-                        <div className="sender">
-                            <span className="text-box">Hi, I'm sender</span>
-                        </div>
-
-                        <div className="recipient ">
-                            <span className="text-box">Hi, I'm recipient</span>
-                        </div>
-                        <div className="sender">
-                            <span className="text-box">Hi, I'm sender</span>
-                        </div>
-
-                        <div className="recipient ">
-                            <span className="text-box">Hi, I'm recipient</span>
-                        </div>
-
-                    </div>
-                    <div className="send-message">
-                        <div className="send-bar">
-                            <textarea
-                                name="message"
-                                className="multiline-input"
-                                id="multiline-input"
-                                placeholder="Your opinion"
-                                value={message}
-                                onChange={handleInputChange}
-                            />
-                            <button>Send</button>
-                        </div>
-                    </div>
-                </form>
-
+  return (
+    <>
+      <div className="box_chatDetails_container">
+        <div className="box_chatDetails_details">
+          <img src={User} alt="image" />
+          <div className="txt_chat_boxdetails">
+            <h4>Sompong</h4>
+          </div>
+        </div>
+        <form className="message-form" onSubmit={handleChat}>
+          <div className="box_chat_content_room">
+            <div className="sender">
+              <span className="text-box">Hi, I'm sender</span>
             </div>
-        </>
-    )
+
+            <div className="recipient ">
+              <span className="text-box">Hi, I'm recipient</span>
+            </div>
+            <div className="sender">
+              <span className="text-box">Hi, I'm sender</span>
+            </div>
+
+            <div className="recipient ">
+              <span className="text-box">Hi, I'm recipient</span>
+            </div>
+            <div className="sender">
+              <span className="text-box">Hi, I'm sender</span>
+            </div>
+
+            <div className="recipient ">
+              <span className="text-box">Hi, I'm recipient</span>
+            </div>
+            <div className="sender">
+              <span className="text-box">Hi, I'm sender</span>
+            </div>
+
+            <div className="recipient ">
+              <span className="text-box">Hi, I'm recipient</span>
+            </div>
+            <div className="sender">
+              <span className="text-box">Hi, I'm sender</span>
+            </div>
+
+            <div className="recipient ">
+              <span className="text-box">Hi, I'm recipient</span>
+            </div>
+          </div>
+          <div className="send-bar">
+            <textarea
+              className="multiline-input"
+              id="multiline-input"
+              placeholder="A-Z ..."
+              rows="1"
+              value={message}
+              onChange={handleInputChange}
+            />
+            <button type="submit">
+              <VscSend />
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default Chat_Details
+export default Chat_Details;
