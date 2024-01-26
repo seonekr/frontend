e2293bcd455215aa8aca0b303d0d7698e198d896
seonekr <zaemuevln42gmail.com>
2024-01-26 12:06:54 +0700

    Chat

--- src/components/chat/Chats.jsx
+++ src/components/chat/Chats.jsx
@@ -1,54 +1,95 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from "react";
 import Header from "../header/Header";
 import Menu from "../menuFooter/Menu";
 import { FaMagnifyingGlass } from "react-icons/fa6";
-import User from '../../img/user.png'
-import './chats.css'
-import { Link } from 'react-router-dom';
+import User from "../../img/user.png";
+import "./chats.css";
+import { Link } from "react-router-dom";
 import { IoMdMore } from "react-icons/io";
 
 const Chats = () => {
+
+  const [isVisible, setIsVisible] = useState(false);
+  const buttonRef = useRef();
+  const divsRef = useRef();
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+  const handleDocumentClick = (e) => {
+    if (
+      buttonRef.current &&
+      !buttonRef.current.contains(e.target) &&
+      divsRef.current &&
+      !divsRef.current.contains(e.target)
+    ) {
+      setIsVisible(false);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener('click', handleDocumentClick);
+    return () => {
+      document.removeEventListener('click', handleDocumentClick);
+    };
+  }, []);
   return (
     <>
       <Header />
       <div className="boc_chat_container">
         <form className="search_box_chat">
           <FaMagnifyingGlass className="iconSearch_chat" />
-          <input type="text" className="input_search_chat" placeholder="Search..."></input>
+          <input
+            type="text"
+            className="input_search_chat"
+            placeholder="Search..."
+          ></input>
         </form>
 
         <div className="conversation_list_box">
-
-          <Link to="/chat-details" className="conversation_item">
-            <div className="conversation_details">
-              <img src={User} alt="image" />
-              <div className='txt_chat_box'>
-                <h4>Sompong</h4>
-                <p>new message...</p>
+          <div className="conversation_item">
+            <Link to="/chat-details">
+              <div className="conversation_details">
+                <img src={User} alt="image" />
+                <div className="txt_chat_box">
+                  <h4>Sompong</h4>
+                  <p>new message...</p>
+                </div>
               </div>
-            </div>
-            <Link to="#" className="conversation_options">
-              <IoMdMore />
             </Link>
-          </Link>
-          <Link to="/chat-details" className="conversation_item">
-            <div className="conversation_details">
-              <img src={User} alt="image" />
-              <div className='txt_chat_box'>
-                <h4>Sompong</h4>
-                <p>new message...</p>
+            <button className="conversation_options" ref={buttonRef} onClick={toggleVisibility}>
+              <IoMdMore />
+            </button>
+            {isVisible && (
+            <div className="del-cls" ref={divsRef}>
+              <div>delete</div>
+              <div>clear</div>
+            </div>
+            )}
+          </div>
+          <div className="conversation_item">
+            <Link to="/chat-details">
+              <div className="conversation_details">
+                <img src={User} alt="image" />
+                <div className="txt_chat_box">
+                  <h4>Sompong</h4>
+                  <p>new message...</p>
+                </div>
               </div>
-            </div>
-            <Link to="#" className="conversation_options">
-              <IoMdMore />
             </Link>
-          </Link>
-
+            <button className="conversation_options" ref={buttonRef} onClick={toggleVisibility}>
+              <IoMdMore />
+            </button>
+            {isVisible && (
+            <div className="del-cls" ref={divsRef}>
+              <div>delete</div>
+              <div>clear</div>
+            </div>
+            )}
+          </div>
         </div>
       </div>
       <Menu />
     </>
-  )
-}
+  );
+};
 
-export default Chats
+export default Chats;
