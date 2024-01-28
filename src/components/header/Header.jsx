a84b48c49cb69cf5aff9972f831efb9ce5fd45ef
seonekr <oudonephengkhamlar@gmail.com>
2024-01-28 12:37:28 +0700

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,81 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./header.css";
 import { HiOutlineBuildingStorefront } from "react-icons/hi2";
 import { FaMagnifyingGlass, FaCartShopping, FaRegUser } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo1 from "../../img/Logo1.png";
+import axios from "axios";
 
 const Header = () => {
+  const token = localStorage.getItem("token");
   const user = localStorage.getItem("user");
+  const navigate = useNavigate();
+  const [search, set_search] = useState("");
+
+  // const [is_logined, set_is_logined] = useState(false)
+
+  useEffect(() => {
+    let data = JSON.stringify({
+      token: token,
+    });
+
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: import.meta.env.VITE_API + "/user/check-token",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      data: data,
+    };
+
+    axios
+      .request(config)
+      .then((response) => {
+        if (response.data.result === "success") {
+          navigate("/");
+          return;
+        } else {
+          localStorage.removeItem("token");
+          localStorage.removeItem("user");
+          navigate("/");
+          return;
+        }
+      })
+      .catch((error) => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        console.log(error);
+      });
+  }, [token]);
+
+  function OnSearch(e) {
+    e.preventDefault();
+    console.log(e);
+    // let data = JSON.stringify({
+    //   search: e,
+    // });
+
+    // let config = {
+    //   method: "post",
+    //   maxBodyLength: Infinity,
+    //   url: "http://127.0.0.1:8000/store/search",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   data: data,
+    // };
+
+    // axios
+    //   .request(config)
+    //   .then((response) => {
+    //     console.log(JSON.stringify(response.data));
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+  }
 
   return (
     <>
@@ -35,13 +104,18 @@ const Header = () => {
             </div>
 
             <div className="ulHead_box">
-              <form className="search_wrap search_wrap_2">
+              <form onSubmit={OnSearch} className="search_wrap search_wrap_2">
                 <div className="search_box">
                   <div className="btn_common">
                     <FaMagnifyingGlass className="iconSearch" />
                   </div>
                   <input
+                    id="search"
                     type="text"
+                    value={search}
+                    onChange={(e) => {
+                      set_search(e.target.value);
+                    }}
                     className="input_search_heaederr"
                     placeholder="search..."
                   ></input>
