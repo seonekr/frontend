da77eece2832c4473782eaaca537bd67dd01495d
seonekr <93129992+KONGCHAN-999@users.noreply.github.com>
2024-01-29 19:18:17 +0700

    Update-chat

--- src/components/chat/Chats.jsx
+++ src/components/chat/Chats.jsx
@@ -1,5 +1,5 @@
 
-import React, {useState, useEffect, useRef} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Header from "../header/Header";
 import Menu from "../menuFooter/Menu";
 import { FaMagnifyingGlass } from "react-icons/fa6";
@@ -10,28 +10,13 @@ import { IoMdMore } from "react-icons/io";
 
 const Chats = () => {
 
-  const [isVisible, setIsVisible] = useState(false);
-  const buttonRef = useRef();
-  const divsRef = useRef();
-  const toggleVisibility = () => {
-    setIsVisible(!isVisible);
+  //PopUp box add banner
+  const [isPopupVisible, setPopupVisible] = useState(false);
+
+  const togglePopup = () => {
+    setPopupVisible(!isPopupVisible);
   };
-  const handleDocumentClick = (e) => {
-    if (
-      buttonRef.current &&
-      !buttonRef.current.contains(e.target) &&
-      divsRef.current &&
-      !divsRef.current.contains(e.target)
-    ) {
-      setIsVisible(false);
-    }
-  };
-  useEffect(() => {
-    document.addEventListener('click', handleDocumentClick);
-    return () => {
-      document.removeEventListener('click', handleDocumentClick);
-    };
-  }, []);
+
   return (
     <>
       <Header />
@@ -45,9 +30,10 @@ const Chats = () => {
           ></input>
         </form>
 
-        <div className="conversation_list_box">
-          <div className="conversation_item">
-            <Link to="/chat-details">
+        <div className="conversation_list_box" >
+
+          <div className="conversation_item_box">
+            <Link to="/chat-details" className="conversation_item">
               <div className="conversation_details">
                 <img src={User} alt="image" />
                 <div className="txt_chat_box">
@@ -55,17 +41,21 @@ const Chats = () => {
                   <p>new message...</p>
                 </div>
               </div>
+              <Link to="#" className="conversation_options" onClick={togglePopup}>
+                <IoMdMore />
+              </Link>
+
             </Link>
-            <button className="conversation_options" ref={buttonRef} onClick={toggleVisibility}>
-              <IoMdMore />
-            </button>
-            {isVisible && (
-            <div className="del-cls phone" ref={divsRef}>
-              <button>delete</button>
-              <button>clear</button>
-            </div>
+            {/* PopUp box add banner */}
+            {isPopupVisible && (
+              <div className="box_alter_delelte_chat">
+                <p>Delete</p>
+                <span className="spanOfborderButtom_popup"></span>
+                <p>Clear chat</p>
+              </div>
             )}
           </div>
+
         </div>
       </div>
       <Menu />
